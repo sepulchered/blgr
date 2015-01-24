bf93025b67b01688eb3fe3a938401d0fc3ab2708
Andrey Dresvyannikov <andrey@dresvyannikov.ru>
2015-01-24 23:37:17 +0300

    create post command added

--- blgr.py
+++ blgr.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3.4
-import argparse
+import os
 import json
+import shutil
+import datetime
+import argparse
 
 
 class Command(type):
@@ -34,8 +37,49 @@ class Create(BlgrCommand):
         self.parser.add_argument('type', metavar='TYPE', help='type of item to create',
                                  choices=['post', 'page'])
 
-    def prepare(self):
+    def _prepare_post(self):
+        resp = None
+        while resp not in ('y', 'n', ''):
+            resp = input('Would you like to provide post metadata now {y/[n]}')
+
+        if resp == 'y':
+            self._ask_post_meta()
+
+        self._create_post()
+
+    def _ask_post_meta(self):
+        post_data = {}
+        post_data['title'] = input('Post title: ')
+        post_data['slug'] = input('Post slug: ')
+        post_data['category'] = input('Post category: ')
+        self.post_data = post_data
+
+    def _create_post(self):
+        dt = datetime.datetime.now()
+        ts = dt.strftime('%Y-%m-%d-%H')
+        slug = self.post_data.setdefault('slug', 'post')
+        post_dir = os.path.join(self.config['posts']['path'],
+                                '-'.join((ts, slug)))
+        post_name = '{}{}.{}'.format(slug, ts, 'ipynb')
+
+        if not os.path.exists(post_dir):
+            os.makedirs(post_dir)
+
+        shutil.copyfile('./data/empty.ipynb', os.path.join(post_dir, post_name))
+        with open(os.path.join(post_dir, 'meta.json'), 'w') as meta:
+            json.dump({'title': self.post_data.setdefault('title', ''),
+                       'slug': self.post_data.setdefault('slug', ''),
+                       'category': self.post_data.setdefault('category'),
+                       'dt': dt.isoformat()}, meta)
+
+    def _prepare_page(self):
         pass
+
+    def prepare(self):
+        if self.type == 'post':
+            self._prepare_post()
+        elif self.type == 'page':
+            self._prepare_page()
 
     def execute(self):
         pass
