0ca0d5d014f2c34392c290311baedfb305af1586
Andrey Dresvyannikov <andrey@dresvyannikov.ru>
2015-01-28 11:38:15 +0300

    changed path to blgr

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,2 +1,11 @@
-from blgr.blgr import
+from blgr import BlgrCli, Create
 
+
+def test_reads_config():
+    cli = BlgrCli()
+    conf_file_path = '../blgr/config.json'
+    cli.process_cli_args(['-c', conf_file_path, 'create'])
+    assert isinstance(cli.cmd, Create)
+    assert cli.cmd.cli_args['config_path'] == conf_file_path
+    cli.read_config()
+    assert cli.cmd.config
