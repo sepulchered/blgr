f1a03225a45a200517db66fb840f29cf1dab20d5
Andrey Dresvyannikov <andrey@dresvyannikov.ru>
2015-01-28 11:50:07 +0300

    could not guess proper import path for codehip to handle it

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,9 +1,9 @@
-from blgr import BlgrCli, Create
+from blgr.blgr import BlgrCli, Create
 
 
 def test_reads_config():
     cli = BlgrCli()
-    conf_file_path = '../blgr/config.json'
+    conf_file_path = 'blgr/config.json'
     cli.process_cli_args(['-c', conf_file_path, 'create'])
     assert isinstance(cli.cmd, Create)
     assert cli.cmd.cli_args['config_path'] == conf_file_path
