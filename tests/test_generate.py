6d739dd1ed18a08aa5c0562ffa40b519595e3595
Andrey Dresvyannikov <andrey@dresvyannikov.ru>
2015-01-30 09:43:17 +0300

    test template for generate command added

---
+++ tests/test_generate.py
@@ -0,0 +1,57 @@
+from blgr.blgr import Generate
+
+
+def test_prepare():
+    pass
+
+
+def test_main_indices():
+    pass
+
+
+def test_pages():
+    pass
+
+
+def test_comments():
+    pass
+
+
+def test_menu():
+    pass
+
+
+def test_year_index():
+    pass
+
+
+def test_month_index():
+    pass
+
+
+def test_day_index():
+    pass
+
+
+def test_category_index():
+    pass
+
+
+def test_generate_categories():
+    pass
+
+
+def test_process_ipynb():
+    pass
+
+
+def test_append_html():
+    pass
+
+
+def test_generate_posts():
+    pass
+
+
+def test_execute():
+    pass
